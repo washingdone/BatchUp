4e0b63a6c9297754c98b3f991f887794fcaaef1c
Marci <yu.gi.ohlover1984@gmail.com>
2022-01-25 10:47:54 -0600

    :lipstick: Set up initial display code

--- app/src/main/java/com/example/batchup/MainActivity.java
+++ app/src/main/java/com/example/batchup/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.batchup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView showTV = findViewById(R.id.showTV);
+        String contents =showTV.getText().toString();
+        contents = "1 cup water\n2 teaspoons lemon juice\n3 tablespoons sugar";
+        showTV.setText(contents);
     }
 }
