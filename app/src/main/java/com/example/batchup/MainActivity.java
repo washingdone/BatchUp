4864ab83932cd3a71f65af74ff0e334e04691017
Marci <yu.gi.ohlover1984@gmail.com>
2022-01-25 13:12:26 -0600

    :construction: :goal_net: Add button function code

--- app/src/main/java/com/example/batchup/MainActivity.java
+++ app/src/main/java/com/example/batchup/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.batchup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +18,29 @@ public class MainActivity extends AppCompatActivity {
         String contents =showTV.getText().toString();
         contents = "1 cup water\n2 teaspoons lemon juice\n3 tablespoons sugar";
         showTV.setText(contents);
+    }
+
+    public void batchButton(){
+        TextView showTV = findViewById(R.id.showTV);
+        EditText multiplyET = findViewById(R.id.multiplyET);
+        String batchNumStr = multiplyET.getText().toString();
+
+        try {
+            int batchNumber =Integer.parseInt(batchNumStr);
+
+            if (batchNumber < 1) {
+                throw new NumberFormatException();
+            } else {
+                String newData = (1 * batchNumber) + " cup water\n" +
+                        (2 * batchNumber) + " teaspoons lemon juice\n" +
+                        (3 * batchNumber) + " tablespoons sugar";
+            }
+        } catch(NumberFormatException e) {
+            // Handle invalid input
+            Toast.makeText(this, "Please input a positive integer", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            // Handle all other exceptions
+            Log.d("Unknown exception: ", e.toString());
+        }
     }
 }
